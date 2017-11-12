15de2056c98bfae72538fa7a932defd9b38a088b
Hoyon Mak <hoyon.mak@gmail.com>
2017-11-12 23:34:26 +0000

    Use Text IO

--- app/Main.hs
+++ app/Main.hs
@@ -2,17 +2,18 @@ module Main where
 
 import           AST
 import           Check
-import qualified Data.Text as T
+import qualified Data.Text    as T
+import qualified Data.Text.IO as T
 import           Parser
 import           System.IO
 
 main :: IO ()
 main = do
-  file <- readFile "test.sml"
-  let ast = parseString $ T.pack file
+  file <- T.readFile "test.sml"
+  let ast = parseString file
   case ast >>= checkNames of
     Right x -> putStr $ showAST x
-    Left e  -> putStrLn "Errors: " >> putErr e
+    Left e  -> putErr "Errors: " >> putErr e
 
 putErr :: T.Text -> IO ()
-putErr e = hPutStr stderr $ T.unpack e
+putErr = T.hPutStr stderr
